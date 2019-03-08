6da961ea4fee40073ed32abbe6e51892107631f6
erdem <github@erdemozkol.com>
2019-03-08 11:58:04 +0000

    implemented get_meta_description template tag

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -1,4 +1,5 @@
 import dateparser
+from bs4 import BeautifulSoup
 
 from django import template
 
@@ -10,3 +11,16 @@ def parse_date(date_string):
     if date_string:
         return dateparser.parse(date_string).strftime('%d %B %Y')
     return None
+
+
+META_DESCRIPTION_TEXT_LENGTH = 150
+
+
+@register.simple_tag
+def get_meta_description(page, **kwargs):
+    description = page.get('article_teaser')
+    if not description:
+        description = ''.join(BeautifulSoup(
+            page.get('article_body_text')
+        ).findAll(text=True))[:META_DESCRIPTION_TEXT_LENGTH]
+    return description
