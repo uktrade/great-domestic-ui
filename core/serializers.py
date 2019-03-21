72a7d4dd8cb0068228b54df2a1eed45b0f18aad9
Will <wrftaylor@gmail.com>
2019-03-21 15:21:19 +0000

    Update serializers.py
    Remove 'import pdb;'

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,4 @@
 def parse_search_results(content):
-    # import pdb; pdb.set_trace()
     results = [hit['_source'] for hit in content['hits']['hits']]
     for result in results:
         if ('content' in result) and (len(result['content']) > 160):
