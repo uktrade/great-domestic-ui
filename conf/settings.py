0a83dc815dcb472524f07e4bb467fba3ecb6f575
erdem <github@erdemozkol.com>
2019-04-05 13:27:04 +0100

    export elements app removed from INSTALLED_APPS

--- conf/settings.py
+++ conf/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'raven.contrib.django.raven_compat',
     'django.contrib.sessions',
     'django.contrib.sitemaps',
-    'export_elements',
     'formtools',
     'corsheaders',
     'directory_constants',
