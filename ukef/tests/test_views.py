7e09290a5f2a4b249ab90fadddc8d42bba905d8e
erdem <github@erdemozkol.com>
2019-04-18 17:37:17 +0300

    implemented test_contact_form_success_view_response

--- ukef/tests/test_views.py
+++ ukef/tests/test_views.py
@@ -13,76 +13,76 @@ from ukef import forms
 @pytest.mark.parametrize(
     'page_url,page_api_response,expected_status_code',
     (
-        (
-            reverse('get-finance'),
-            {
-                'title': 'UK EXPORT FINANCE',
-                'meta': {
-                    'slug': 'get-finance'
-                }
-            },
-            200
-        ),
-        (
-            reverse('project-finance'),
-            {
-                'title': 'UKEF LANDING',
-                'meta': {
-                    'slug': 'project-finance'
-                }
-            },
-            200
-        ),
-        (
-            reverse('uk-export-contact'),
-            {
-                'title': 'Get in touch',
-                'meta': {
-                    'slug': 'uk-export-contact-form'
-                }
-            },
-            200
-        ),
-        (
-            reverse('uk-export-contract-success'),
-            {
-                'title': 'Get in touch Success',
-                'meta': {
-                    'slug': 'uk-export-contact-form-success'
-                }
-            },
-            302
-        ),
-        (
-            reverse('how-we-assess-your-project'),
-            {
-                'title': 'How we assess your project',
-                'meta': {
-                    'slug': 'how-we-assess-your-project'
-                }
-            },
-            200
-        ),
-        (
-            reverse('country-cover'),
-            {
-                'title': 'Country Cover',
-                'meta': {
-                    'slug': 'country-cover'
-                }
-            },
-            200
-        ),
-        (
-            reverse('what-we-offer-you'),
-            {
-                'title': 'What we offer you',
-                'meta': {
-                    'slug': 'what-we-offer-you'
-                }
-            },
-            200
-        ),
+            (
+                    reverse('get-finance'),
+                    {
+                        'title': 'UK EXPORT FINANCE',
+                        'meta': {
+                            'slug': 'get-finance'
+                        }
+                    },
+                    200
+            ),
+            (
+                    reverse('project-finance'),
+                    {
+                        'title': 'UKEF LANDING',
+                        'meta': {
+                            'slug': 'project-finance'
+                        }
+                    },
+                    200
+            ),
+            (
+                    reverse('uk-export-contact'),
+                    {
+                        'title': 'Get in touch',
+                        'meta': {
+                            'slug': 'uk-export-contact-form'
+                        }
+                    },
+                    200
+            ),
+            (
+                    reverse('uk-export-contract-success'),
+                    {
+                        'title': 'Get in touch Success',
+                        'meta': {
+                            'slug': 'uk-export-contact-form-success'
+                        }
+                    },
+                    302
+            ),
+            (
+                    reverse('how-we-assess-your-project'),
+                    {
+                        'title': 'How we assess your project',
+                        'meta': {
+                            'slug': 'how-we-assess-your-project'
+                        }
+                    },
+                    200
+            ),
+            (
+                    reverse('country-cover'),
+                    {
+                        'title': 'Country Cover',
+                        'meta': {
+                            'slug': 'country-cover'
+                        }
+                    },
+                    200
+            ),
+            (
+                    reverse('what-we-offer-you'),
+                    {
+                        'title': 'What we offer you',
+                        'meta': {
+                            'slug': 'what-we-offer-you'
+                        }
+                    },
+                    200
+            ),
 
     )
 )
@@ -102,7 +102,7 @@ def test_ukef_views_status_code(
 @mock.patch.object(views.ContactView, 'form_session_class')
 @mock.patch.object(forms.UKEFContactForm, 'save')
 def test_contact_form_notify_success(
-    mock_save, mock_form_session, client, valid_contact_form_data
+        mock_save, mock_form_session, client, valid_contact_form_data
 ):
     url = reverse('uk-export-contact')
     response = client.post(url, valid_contact_form_data)
@@ -125,3 +125,14 @@ def test_contact_form_notify_success(
             template_id=settings.UKEF_CONTACT_USER_NOTIFY_TEMPLATE_ID
         )
     ]
+
+
+def test_contact_form_success_view_response(rf):
+    user_email = 'test@test.com'
+    request = rf.get(reverse('uk-export-contract-success'))
+    request.sso_user = None
+    request.session = {'user_email': user_email}
+    view = views.SuccessPageView.as_view()
+    response = view(request)
+    assert response.status_code == 200
+    assert user_email in response.rendered_content
