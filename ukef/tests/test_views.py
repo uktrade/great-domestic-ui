fce0b92f9fcb6b7e879b96d58d69c36cab80efb8
erdem <github@erdemozkol.com>
2019-04-24 19:12:14 +0300

    added test cases for UKEF feature flags

--- ukef/tests/test_views.py
+++ ukef/tests/test_views.py
@@ -3,22 +3,38 @@ from django.urls import reverse
 
 
 @pytest.mark.parametrize(
-    'page_url,page_api_response,expected_status_code',
+    'page_url,page_content,expected_status_code',
     (
         (
             reverse('get-finance'),
             {
-                'title': 'UK EXPORT FINANCE',
-                'meta': {
-                    'slug': 'get-finance'
-                }
+                'title': 'UK Export finance - great.gov.uk',
             },
             200
         ),
     )
 )
-def test_ukef_views_status_code(
-    client, page_url, page_api_response, expected_status_code
+def test_ukef_views_while_feature_flag_enabled(
+    client, page_url, page_content, expected_status_code, settings
 ):
+    settings.FEATURE_FLAGS['UKEF_PAGES_ON'] = True
+    response = client.get(page_url)
+    assert response.status_code == expected_status_code
+    assert page_content['title'] in str(response.rendered_content)
+
+
+@pytest.mark.parametrize(
+    'page_url,expected_status_code',
+    (
+        (
+            reverse('get-finance'),
+            404
+        ),
+    )
+)
+def test_ukef_views_while_feature_flag_disabled(
+    client, page_url, expected_status_code, settings
+):
+    settings.FEATURE_FLAGS['UKEF_PAGES_ON'] = False
     response = client.get(page_url)
     assert response.status_code == expected_status_code
