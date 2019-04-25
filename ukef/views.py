2169db9e13f625ccb08d311fbf23b829342a6788
erdem <github@erdemozkol.com>
2019-04-25 14:58:01 +0300

    implemented UKEF project finance page

--- ukef/views.py
+++ ukef/views.py
@@ -5,3 +5,7 @@ from ukef.mixins import UKEFPagesFeatureFlagMixin
 
 class HomeView(UKEFPagesFeatureFlagMixin, TemplateView):
     template_name = 'ukef/home_page.html'
+
+
+class LandingView(UKEFPagesFeatureFlagMixin, TemplateView):
+    template_name = 'ukef/landing_page.html'
