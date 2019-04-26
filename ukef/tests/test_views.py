d0b1f40d8f7db2a6281ad1168861fc91f5922b29
erdem <github@erdemozkol.com>
2019-04-26 16:03:20 +0300

    improved ukef/test_views

--- ukef/tests/test_views.py
+++ ukef/tests/test_views.py
@@ -25,6 +25,13 @@ from ukef import forms
             },
             200
         ),
+        (
+            reverse('uk-export-contact'),
+            {
+                'title': 'UK Export finance contact form - great.gov.uk',
+            },
+            200
+        ),
     )
 )
 def test_ukef_views_while_feature_flag_enabled(
@@ -45,6 +52,10 @@ def test_ukef_views_while_feature_flag_enabled(
         ),
         (
             reverse('project-finance'),
+            404
+        ),
+        (
+            reverse('uk-export-contact'),
             404
         ),
     )
