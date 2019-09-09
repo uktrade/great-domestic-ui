0fb7e89b2937a78da358fc5bac735c43d196756d
Jeanette Murphy <jeanettemurphy@meggaweb.co.uk>
2019-09-09 11:10:26 +0100

    GTRANSFORM-238 use page heading for title followed by site

---
+++ contact/tests/test_templates.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import patch
+from bs4 import BeautifulSoup
+from django.template.loader import render_to_string
+
+from core.tests.helpers import create_response
+
+
+def test_cms_guidance_descriptive_page_title_is_rendered(rf):
+
+    context = {
+        'request': rf.get('/'),
+    }
+    page = {
+        'title': 'Descriptive text',
+    }
+
+    context['page'] = page
+    html = render_to_string('contact/guidance.html', context)
+
+    assert page['title'] + ' - great.gov.uk' in html
+
+
+def test_contact_domestic_descriptive_page_title_override_is_rendered():
+
+    html = render_to_string('contact/domestic/step.html')
+
+    assert 'Tell us how we can help - great.gov.uk' in html
+
+
+
