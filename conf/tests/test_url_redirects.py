ce26219c521959658347a0686a3ffda998210ac8
richtier <contact@richardtier.co.uk>
2020-01-30 16:23:07 +0000

    Change brexit urls to transition-period

--- conf/tests/test_url_redirects.py
+++ conf/tests/test_url_redirects.py
@@ -789,6 +789,14 @@ def test_redirects_no_trailing_slash(url, expected, client):
                 '/regular/',
                 '/advice'
         ),
+        (
+            '/brexit/contact/',
+            '/transition-period/contact/',
+        ),
+        (
+            '/brexit/contact/success/',
+            '/transition-period/contact/success/',
+        )
     ])
 def redirect_articles(incoming_url, expected_url, client):
     response = client.get(incoming_url)
