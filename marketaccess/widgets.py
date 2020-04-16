01389d4e7deb57fcfb8dad55b4ba0e18e5bcd082
richtier <contact@richardtier.co.uk>
2020-04-16 18:18:54 +0100

    Add Covid 19 option to /report-trade-barrier/report/problem-details/

---
+++ marketaccess/widgets.py
@@ -0,0 +1,5 @@
+from directory_components import forms
+
+
+class TickboxWithOptionsHelpText(forms.CheckboxSelectInlineLabelMultiple):
+    option_template_name = 'marketaccess/report_barrier_form/tickbox-with-options-helptext.html'
