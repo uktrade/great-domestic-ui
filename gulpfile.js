0dbe51320d8a96639040248fd66477b303632586
Le H Ngo <hgle.ngo@gmail.com>
2021-08-19 17:55:44 +0100

    - updates to gulp 5 to fix tar vuln

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'))
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
 
